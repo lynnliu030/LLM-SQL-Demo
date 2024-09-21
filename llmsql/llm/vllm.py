27dcf801e90379d87373245e5237d0b613b7c40b
Asim Biswal <abiswal@berkeley.edu>
2024-09-20 19:56:30 -0700

    server support

--- llmsql/llm/vllm.py
+++ llmsql/llm/vllm.py
@@ -10,21 +10,31 @@ except ImportError as e:
 from vllm import EngineArgs, LLM as LLMEntrypoint, SamplingParams
 
 from llmsql.llm.base import DEFAULT_SYSTEM_PROMPT, LLM
+from llmsql.utils import post_http_request, is_server_running, get_tokenizer
 
 class vLLM(LLM):
-    def __init__(self, engine_args: EngineArgs, sampling_params: Optional[SamplingParams] = None):
-        print("Starting vLLM engine...")
-
-        self.engine_args = engine_args
-        self.sampling_params = sampling_params or SamplingParams()
-        
-        # Enable prefix caching.
-        self.engine_args.enable_prefix_caching = True
-        # Disable log stats by default
-        self.engine_args.disable_log_stats = True
-
-        self.engine = LLMEntrypoint(**asdict(self.engine_args))
-        self.tokenizer = self.engine.get_tokenizer()
+    def __init__(self, engine_args: EngineArgs, sampling_params: Optional[SamplingParams] = None, base_url: Optional[str] = None):
+        self.base_url = None
+        if base_url:
+            model = is_server_running(base_url)
+            if model:
+                self.base_url = base_url
+                self.model = model
+                self.tokenizer = get_tokenizer()
+                print(f"Connected to vLLM server running at {self.base_url}")
+            else:
+                print("vLLM server connection error")
+        else:
+            print("Starting vLLM engine...")
+            self.engine_args = engine_args
+            self.sampling_params = sampling_params or SamplingParams()
+            
+            # Enable prefix caching.
+            self.engine_args.enable_prefix_caching = True
+            # Disable log stats by default
+            self.engine_args.disable_log_stats = True
+            self.engine = LLMEntrypoint(**asdict(self.engine_args))
+            self.tokenizer = self.engine.get_tokenizer()
 
     def _generate_prompt(self, user_prompt: str, system_prompt: str) -> str:
         messages = [
@@ -59,7 +69,10 @@ class vLLM(LLM):
         user_prompt = f"Answer the below query:\n\n{query}\n\ngiven the following data:\n\n{fields_json}"
         
         prompt = self._generate_prompt(user_prompt=user_prompt, system_prompt=system_prompt)
-        output = self.engine.generate(prompts=[prompt], sampling_params=self.sampling_params, use_tqdm=False)
+        if self.base_url:
+            output = json.loads(post_http_request(self.model, [prompt], temperature=0).content)
+        else:
+            output = self.engine.generate(prompts=[prompt], sampling_params=self.sampling_params, use_tqdm=False)
         assert len(output) == 1
         return output[0].outputs[-1].text
 
@@ -73,8 +86,10 @@ class vLLM(LLM):
         user_prompts = [user_prompt_template.replace("{{fields_json}}", fields_json) for fields_json in fields_json_list]
 
         prompts = [self._generate_prompt(user_prompt=user_prompt, system_prompt=system_prompt) for user_prompt in user_prompts]
-
-        request_outputs = self.engine.generate(prompts=prompts, sampling_params=self.sampling_params)
+        if self.base_url:
+            request_outputs = json.loads(post_http_request(self.model, prompts, temperature=0).content)
+        else:
+            request_outputs = self.engine.generate(prompts=prompts, sampling_params=self.sampling_params)
         assert len(request_outputs) == len(fields)
 
         return [output.outputs[-1].text for output in request_outputs]
--- llmsql/pandas/utils.py
+++ llmsql/pandas/utils.py
@@ -4,6 +4,46 @@ import re
 from pandas import DataFrame
 
 from llmsql.llm.base import DEFAULT_SYSTEM_PROMPT, LLM
+from transformers import AutoTokenizer
+
+def get_tokenizer():
+    return AutoTokenizer.from_pretrained("hf-internal-testing/llama-tokenizer")
+
+def post_http_request(
+    model: str,
+    prompts: List[str],
+    temperature: float = 1,
+    api_url: str = "http://localhost:8000/v1/completions",
+    guided_choice: List[str] = None,
+) -> requests.Response:
+    if guided_choice:
+        pload = {
+            "model": model,
+            "prompt": prompts,
+            "temperature": temperature,
+            "max_tokens": 5,
+            "guided_choice": guided_choice,
+        }
+    else:
+        pload = {
+            "model": model,
+            "prompt": prompts,
+            "temperature": temperature,
+            "max_tokens": 5,
+        }
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(api_url, headers=headers, data=json.dumps(pload))
+    return response
+
+def is_server_running(url="http://localhost:8000/v1/models"):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            model = response.json()['data'][0]['id']
+            return model
+    except requests.ConnectionError:
+        return None
+    return None
 
 def get_fields(user_prompt: str) -> str:
     """Get the names of all the fields specified in the user prompt."""
